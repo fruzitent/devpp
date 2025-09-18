5df99dd03c7f699e0765cbd63ef0f18687f57436
fruzitent <fruzit@gmail.com>
2025-09-18 10:18:06 +0300

    feat: generate types from schema

---
+++ crates/devpp/src/devcontainer.rs
@@ -0,0 +1,29 @@
+pub mod generated {
+    // @see: https://raw.githubusercontent.com/devcontainers/spec/113500f4125e0f14c9293adf4d8d94a861e0ec11/schemas/devContainer.base.schema.json
+    typify::import_types!("./src/devcontainer.schema.json");
+}
+
+// TODO: https://github.com/oxidecomputer/typify/issues/896
+// diff --git a/devcontainer.schema.json b/devcontainer.schema.json
+// index 0000000..0000000 100644
+// --- a/devcontainer.schema.json
+// +++ b/devcontainer.schema.json
+// @@ -451,18 +451,6 @@
+//                                                 },
+//                                                 "gpu": {
+//                                                         "oneOf": [
+// -                                                               {
+// -                                                                       "type": [
+// -                                                                               "boolean",
+// -                                                                               "string"
+// -                                                                       ],
+// -                                                                       "enum": [
+// -                                                                               true,
+// -                                                                               false,
+// -                                                                               "optional"
+// -                                                                       ],
+// -                                                                       "description": "Indicates whether a GPU is required. The string \"optional\" indicates that a GPU is optional. An object value can be used to configure more detailed requirements."
+// -                                                               },
+//                                                                 {
+//                                                                         "type": "object",
+//                                                                         "properties": {
---
+++ crates/devpp/src/feature.rs
@@ -0,0 +1,4 @@
+pub mod generated {
+    // @see: https://raw.githubusercontent.com/devcontainers/spec/113500f4125e0f14c9293adf4d8d94a861e0ec11/schemas/devContainerFeature.schema.json
+    typify::import_types!("./src/feature.schema.json");
+}
