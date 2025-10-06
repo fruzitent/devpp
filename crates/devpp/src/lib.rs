e7fe45e3324bc7f4b1e6474bafb3563a95652db5
fruzitent <fruzit@gmail.com>
2025-10-06 19:29:20 +0300

    chore: remove dbg! output

--- crates/devpp/src/lib.rs
+++ crates/devpp/src/lib.rs
@@ -16,24 +16,13 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 pub fn build(workspace: &std::path::Path, config: Option<&std::path::Path>) -> Result<()> {
     let config = devpp_spec::devc::Config::find_config(workspace, config)?;
-    dbg!(&config);
-    let dotdev = config.find_dotdev()?;
-    dbg!(&dotdev);
     let devc = devpp_spec::devc::DevContainer::new(std::fs::read_to_string(&config.path)?)?;
-    dbg!(&devc);
-
     let build_info = devpp_spec::devc::BuildInfo::new(&config, &devc)?;
-    dbg!(&build_info);
-
     for (id, options) in &devc.common.features {
         let reference = devpp_spec::feat::Reference::new(id, &config)?;
-        dbg!(&reference);
         let feature = devpp_spec::feat::Feature::new(&reference)?;
-        dbg!(&feature);
-
         devpp_containerfile::write_feature(&mut std::io::stdout(), &build_info, &config, &devc, &feature, options)?;
         writeln!(&mut std::io::stdout())?;
     }
-
     Err(Error::NotImplemented)
 }
