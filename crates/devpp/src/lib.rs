c215be8ba6d3cec95af950613832aee93f2fcd54
fruzitent <fruzit@gmail.com>
2025-10-06 23:28:12 +0300

    feat: sort features topologically

--- crates/devpp/src/lib.rs
+++ crates/devpp/src/lib.rs
@@ -8,6 +8,8 @@ pub enum Error {
     DevppSpec(#[from] devpp_spec::Error),
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    #[error("unexpected cycle")]
+    GraphCycle,
     #[error("not implemented")]
     NotImplemented,
 }
@@ -18,11 +20,27 @@ pub fn build(workspace: &std::path::Path, config: Option<&std::path::Path>) -> R
     let config = devpp_spec::devc::Config::find_config(workspace, config)?;
     let devc = devpp_spec::devc::DevContainer::new(std::fs::read_to_string(&config.path)?)?;
     let build_info = devpp_spec::devc::BuildInfo::new(&config, &devc)?;
+
+    let mut features = std::collections::BTreeMap::new();
+    let mut graph = topo_sort::TopoSort::new();
+
     for (id, options) in &devc.common.features {
         let reference = devpp_spec::feat::Reference::new(id, &config)?;
         let feature = devpp_spec::feat::Feature::new(&reference)?;
-        devpp_containerfile::write_feature(&mut std::io::stdout(), &build_info, &config, &devc, &feature, options)?;
+        graph.insert(id.to_owned(), feature.inner.installs_after.to_vec());
+        features.insert(id, (feature, options));
+    }
+
+    let items = match graph.into_vec_nodes() {
+        topo_sort::SortResults::Full(items) => items,
+        topo_sort::SortResults::Partial(_) => return Err(Error::GraphCycle),
+    };
+
+    for id in items {
+        let (feature, options) = features.get(&id).unwrap();
+        devpp_containerfile::write_feature(&mut std::io::stdout(), &build_info, &config, &devc, feature, options)?;
         writeln!(&mut std::io::stdout())?;
     }
+
     Err(Error::NotImplemented)
 }
